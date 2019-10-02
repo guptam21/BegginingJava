8d7beb97e335db2fd738d550c5e71bb0f5ed5645
guptam21 <mahekg00@gmail.com>
2019-10-02 10:57:19 -0500

    i hat

---
+++ src/gridPoint.java
@@ -0,0 +1,14 @@
+/*
+Mahek Gupta
+9/24/19
+ */
+public class gridPoint
+{
+    public static void main(String[] args)
+    {
+       grid  g = new grid();
+       System.out.println(g);
+       System.out.println();
+       System.out.println("        .");
+    }
+}
---
+++ src/spider.java
@@ -0,0 +1,24 @@
+/*9/20/19
+*/
+public class spider {
+
+    public static void main (String [] args)
+    {
+        top();
+        low();
+
+    }
+    public static void top()
+    {
+        System.out.println("  \\/");
+        System.out.println(" \\\\//");
+        System.out.println("\\\\\\///");
+    }
+    public static void low()
+    {
+        System.out.println("///\\\\\\");
+        System.out.println(" //\\\\");
+        System.out.println("  /\\");
+
+    }
+}
